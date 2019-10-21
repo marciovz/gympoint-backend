9a77a248165b0aed1236ed6c99af01dd883d1798
marciovz <marciovz@yahoo.com.br>
2019-10-21 18:53:05 -0300

    Criado as migrações, rotas, modelo e controller para students

---
+++ src/app/controllers/StundentController.js
@@ -0,0 +1,131 @@
+import * as Yup from 'yup';
+
+import Student from '../models/Student';
+
+class StudentController {
+  // store, show, index, update, delete
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      age: Yup.number()
+        .positive()
+        .integer()
+        .required(),
+      weight: Yup.number()
+        .positive()
+        .required(),
+      height: Yup.number()
+        .positive()
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Fails ' });
+    }
+
+    const studentExists = await Student.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (studentExists) {
+      return res.status(400).json({ error: 'Email already exists.' });
+    }
+
+    try {
+      const student = await Student.create(req.body);
+      return res.json(student);
+    } catch (err) {
+      return res.status(500).json({ error: 'Can not create in database.' });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const students = await Student.findAll();
+      return res.json(students);
+    } catch (err) {
+      return res.status(500).json({ error: 'Can not read database.' });
+    }
+  }
+
+  async index(req, res) {
+    const { id } = req.params;
+
+    try {
+      const student = await Student.findByPk(id);
+
+      if (!student) {
+        return res.status(400).json({ error: 'Student not found.' });
+      }
+
+      return res.json(student);
+    } catch (err) {
+      return res.status(500).json({ error: 'Can not read database.' });
+    }
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      age: Yup.number()
+        .positive()
+        .integer(),
+      weight: Yup.number().positive(),
+      height: Yup.number().positive(),
+    });
+
+    if (!schema.isValid(req.body)) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    const { id } = req.params;
+    try {
+      const student = await Student.findByPk(id);
+
+      if (!student) {
+        return res.status(400).json({ error: 'Student not found.' });
+      }
+
+      const { email } = req.body;
+      if (email && email !== student.email) {
+        const otherStudent = await Student.findOne({
+          where: { email: req.body.email },
+        });
+
+        if (otherStudent) {
+          return res.status(400).json({ error: 'Email already exists.' });
+        }
+      }
+
+      await student.update(req.body);
+      return res.json(student);
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ error: 'Can not update student in database.' });
+    }
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+    try {
+      const student = await Student.findByPk(id);
+
+      if (!student) {
+        return res.status(400).json({ error: 'Student not found.' });
+      }
+      await student.destroy();
+      return res.send();
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ error: 'Can not delete student in database.' });
+    }
+  }
+}
+
+export default new StudentController();
--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,20 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StundentController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
-
+// sessions
 routes.post('/sessions', SessionController.store);
-
-// rotas autenticadas
-routes.use(authMiddleware);
-routes.get('/users', UserController.show);
+// users
+routes.get('/users', authMiddleware, UserController.show);
+// students
+routes.post('/students', authMiddleware, StudentController.store);
+routes.get('/students', authMiddleware, StudentController.show);
+routes.get('/students/:id', StudentController.index);
+routes.put('/students/:id', StudentController.update);
+routes.delete('/students/:id', authMiddleware, StudentController.delete);
 
 export default routes;
